9dafed6da28cbff618f4d3803afca8e7be10c1c8
Thotogelo <49166912+Thotogelo@users.noreply.github.com>
2024-06-27 15:12:33 +0200

    Implement repository

---
+++ Survey_backend/DBConfig/SurveyDatabaseSettings.cs
@@ -0,0 +1,10 @@
+﻿namespace Survey_backend.Model;
+
+public class SurveyDatabaseSettings
+{
+    public string ConnectionString { get; set; } = null!;
+
+    public string DatabaseName { get; set; } = null!;
+
+    public string SurveyCollectionName { get; set; } = null!;
+}
---
+++ Survey_backend/Service/SurveyRepository.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.Extensions.Options;
+using MongoDB.Driver;
+using Survey_backend.Model;
+
+namespace Survey_backend.Service;
+
+public class SurveyRepository
+{
+    private readonly IMongoCollection<SurveyDTO> _surveyCollection;
+    private readonly IMongoCollection<UserDTO> _userCollection;
+
+    public SurveyRepository(IOptions<SurveyDatabaseSettings> bookStoreDatabaseSettings)
+    {
+        var mongoClient = new MongoClient(
+            bookStoreDatabaseSettings.Value.ConnectionString);
+
+        var mongoDatabase = mongoClient.GetDatabase(
+            bookStoreDatabaseSettings.Value.DatabaseName);
+
+        _surveyCollection = mongoDatabase.GetCollection<SurveyDTO>(
+            bookStoreDatabaseSettings.Value.SurveyCollectionName);
+    }
+
+    public IEnumerable<SurveyDTO> GetByType(string type) =>
+        _surveyCollection.Find(survey => survey.Type == type).ToList();
+
+    public SurveyDTO verifySurvey(string userId, string type) =>
+        _surveyCollection.Find(survey => survey.User_id == userId && survey.Type == type).FirstOrDefault();
+
+    public UserDTO verfiyUser(string userId) =>
+        _userCollection.Find(user => user.Id == userId).FirstOrDefault();
+}
