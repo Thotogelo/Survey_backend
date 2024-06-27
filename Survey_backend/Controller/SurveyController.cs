18ae010464e7635c1f1b19d38a9eeca6f290fea4
Thotogelo <49166912+Thotogelo@users.noreply.github.com>
2024-06-27 15:26:12 +0200

    Implement Controller

---
+++ Survey_backend/Controller/SurveyController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Survey_backend.Model;
+using Survey_backend.Service;
+
+namespace Survey_backend.Controller;
+
+[ApiController]
+[Route("api/[controller]")]
+public class SurveyController : ControllerBase
+{
+    private readonly SurveyRepository _surveyRepository;
+
+    public SurveyController(SurveyRepository surveyRepository)
+    {
+        _surveyRepository = surveyRepository;
+    }
+
+    [HttpPost]
+    public IActionResult CreateSurvey([FromBody] SurveyDTO surveyDTO)
+    {
+        try
+        {
+            _surveyRepository.CreateSurvey(surveyDTO);
+            return Ok("Survey created successfully");
+        }
+        catch (Exception e)
+        {
+            return BadRequest(e.Message);
+        }
+    }
+
+    [HttpGet]
+    public IActionResult GetSurveys(string type)
+    {
+        try
+        {
+            var surveys = _surveyRepository.GetByType(type);
+            return Ok(surveys);
+        }
+        catch (Exception e)
+        {
+            return BadRequest(e.Message);
+        }
+    }
+}
